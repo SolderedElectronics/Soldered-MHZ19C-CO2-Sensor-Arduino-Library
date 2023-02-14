a99519a9fedc66371b5608e152fa587485c6c902
Docs Robot <robot@example.com>
2023-02-14 15:45:46 +0000

    Committing documentation changes

--- docs/dir_1e40497411da8229b60f25be84124cca.js
+++ docs/dir_1e40497411da8229b60f25be84124cca.js
@@ -1,36 +1,8 @@
 var dir_1e40497411da8229b60f25be84124cca =
 [
-    [ "circular_queue.h", "circular__queue_8h.html", [
-      [ "circular_queue< T, ForEachArg >", "classcircular__queue.html", "classcircular__queue" ]
-    ] ],
-    [ "circular_queue_mp.h", "circular__queue__mp_8h.html", [
-      [ "circular_queue_mp< T, ForEachArg >", "classcircular__queue__mp.html", "classcircular__queue__mp" ]
-    ] ],
-    [ "Delegate.h", "_delegate_8h.html", [
-      [ "delegate::detail::DelegatePImpl< A, R, P >", "classdelegate_1_1detail_1_1_delegate_p_impl.html", "classdelegate_1_1detail_1_1_delegate_p_impl" ],
-      [ "delegate::detail::DelegatePImpl< void, R, P... >", "classdelegate_1_1detail_1_1_delegate_p_impl_3_01void_00_01_r_00_01_p_8_8_8_01_4.html", "classdelegate_1_1detail_1_1_delegate_p_impl_3_01void_00_01_r_00_01_p_8_8_8_01_4" ],
-      [ "delegate::detail::DelegateImpl< A, R >", "classdelegate_1_1detail_1_1_delegate_impl.html", "classdelegate_1_1detail_1_1_delegate_impl" ],
-      [ "delegate::detail::DelegateImpl< void, R >", "classdelegate_1_1detail_1_1_delegate_impl_3_01void_00_01_r_01_4.html", "classdelegate_1_1detail_1_1_delegate_impl_3_01void_00_01_r_01_4" ],
-      [ "delegate::detail::Delegate< A, R, P >", "classdelegate_1_1detail_1_1_delegate.html", "classdelegate_1_1detail_1_1_delegate" ],
-      [ "delegate::detail::Delegate< A *, R, P... >", "classdelegate_1_1detail_1_1_delegate_3_01_a_01_5_00_01_r_00_01_p_8_8_8_01_4.html", "classdelegate_1_1detail_1_1_delegate_3_01_a_01_5_00_01_r_00_01_p_8_8_8_01_4" ],
-      [ "delegate::detail::Delegate< void, R, P... >", "classdelegate_1_1detail_1_1_delegate_3_01void_00_01_r_00_01_p_8_8_8_01_4.html", "classdelegate_1_1detail_1_1_delegate_3_01void_00_01_r_00_01_p_8_8_8_01_4" ],
-      [ "delegate::detail::Delegate< A, R >", "classdelegate_1_1detail_1_1_delegate_3_01_a_00_01_r_01_4.html", "classdelegate_1_1detail_1_1_delegate_3_01_a_00_01_r_01_4" ],
-      [ "delegate::detail::Delegate< A *, R >", "classdelegate_1_1detail_1_1_delegate_3_01_a_01_5_00_01_r_01_4.html", "classdelegate_1_1detail_1_1_delegate_3_01_a_01_5_00_01_r_01_4" ],
-      [ "delegate::detail::Delegate< void, R >", "classdelegate_1_1detail_1_1_delegate_3_01void_00_01_r_01_4.html", "classdelegate_1_1detail_1_1_delegate_3_01void_00_01_r_01_4" ],
-      [ "Delegate< R(P...), A >", "class_delegate_3_01_r_07_p_8_8_8_08_00_01_a_01_4.html", "class_delegate_3_01_r_07_p_8_8_8_08_00_01_a_01_4" ],
-      [ "Delegate< R(P...)>", "class_delegate_3_01_r_07_p_8_8_8_08_4.html", "class_delegate_3_01_r_07_p_8_8_8_08_4" ]
-    ] ],
+    [ "circular_queue.h", "circular__queue_8h.html", "circular__queue_8h" ],
+    [ "circular_queue_mp.h", "circular__queue__mp_8h.html", "circular__queue__mp_8h" ],
+    [ "Delegate.h", "_delegate_8h.html", "_delegate_8h" ],
     [ "ghostl.h", "ghostl_8h.html", "ghostl_8h" ],
-    [ "MultiDelegate.h", "_multi_delegate_8h.html", [
-      [ "InterruptLock", "class_interrupt_lock.html", "class_interrupt_lock" ],
-      [ "delegate::detail::MultiDelegatePImpl< Delegate, R, ISQUEUE, QUEUE_CAPACITY, P >", "classdelegate_1_1detail_1_1_multi_delegate_p_impl.html", "classdelegate_1_1detail_1_1_multi_delegate_p_impl" ],
-      [ "delegate::detail::MultiDelegatePImpl< Delegate, R, ISQUEUE, QUEUE_CAPACITY, P >::Node_t", "structdelegate_1_1detail_1_1_multi_delegate_p_impl_1_1_node__t.html", "structdelegate_1_1detail_1_1_multi_delegate_p_impl_1_1_node__t" ],
-      [ "delegate::detail::MultiDelegatePImpl< Delegate, R, ISQUEUE, QUEUE_CAPACITY, P >::iterator", "classdelegate_1_1detail_1_1_multi_delegate_p_impl_1_1iterator.html", "classdelegate_1_1detail_1_1_multi_delegate_p_impl_1_1iterator" ],
-      [ "delegate::detail::MultiDelegateImpl< Delegate, R, ISQUEUE, QUEUE_CAPACITY >", "classdelegate_1_1detail_1_1_multi_delegate_impl.html", "classdelegate_1_1detail_1_1_multi_delegate_impl" ],
-      [ "delegate::detail::MultiDelegate< Delegate, R(P...), ISQUEUE, QUEUE_CAPACITY >", "classdelegate_1_1detail_1_1_multi_delegate_3_01_delegate_00_01_r_07_p_8_8_8_08_00_01_i_s_q_u_e_uca5908e0a8f0a8785bc37b46584aa5b7.html", null ],
-      [ "delegate::detail::MultiDelegate< Delegate, R(), ISQUEUE, QUEUE_CAPACITY >", "classdelegate_1_1detail_1_1_multi_delegate_3_01_delegate_00_01_r_07_08_00_01_i_s_q_u_e_u_e_00_01f4d0d6c348447e0c221bc72f5d5ee8eb.html", null ],
-      [ "delegate::detail::MultiDelegate< Delegate, void(P...), ISQUEUE, QUEUE_CAPACITY >", "classdelegate_1_1detail_1_1_multi_delegate_3_01_delegate_00_01void_07_p_8_8_8_08_00_01_i_s_q_u_e977e230ef1939d4be6845cd824f88d38.html", "classdelegate_1_1detail_1_1_multi_delegate_3_01_delegate_00_01void_07_p_8_8_8_08_00_01_i_s_q_u_e977e230ef1939d4be6845cd824f88d38" ],
-      [ "delegate::detail::MultiDelegate< Delegate, void(), ISQUEUE, QUEUE_CAPACITY >", "classdelegate_1_1detail_1_1_multi_delegate_3_01_delegate_00_01void_07_08_00_01_i_s_q_u_e_u_e_00_be7da3c18d87902b0a9838703ad4f54e.html", "classdelegate_1_1detail_1_1_multi_delegate_3_01_delegate_00_01void_07_08_00_01_i_s_q_u_e_u_e_00_be7da3c18d87902b0a9838703ad4f54e" ],
-      [ "MultiDelegate< Delegate, ISQUEUE, QUEUE_CAPACITY >", "class_multi_delegate.html", null ]
-    ] ]
+    [ "MultiDelegate.h", "_multi_delegate_8h.html", "_multi_delegate_8h" ]
 ];
